e4abf59c94ff14eb93e48d035e4e52e52c42baf2
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2019-02-03 00:33:48 +0530

    Updated

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 from flask import Flask
+from gtts import gTTS
+from pygame import mixer
+app = Flask(__name__)
 
+
+@app.route('/')
+def info():
+    html = "<h3>Hello World!</h3>"
+    return html
+
+
+@app.route('/textToSpeech')
+def text_to_speech():
+
+    gTTS("will return a speech from given text").save(".//tts.mp3")
+    return "saved an mp3, please check"
+
+
+@app.route('/<texttospeech>')
+def runtime_text_to_speech(texttospeech):
+    mp3file = './/'+texttospeech+'.mp3'
+    gTTS(texttospeech).save(mp3file)
+    mixer.init()
+    mixer.music.load(mp3file)
+    mixer.music.play()
+    return texttospeech
+
+
+if __name__ == '__main__':
+    app.run()
